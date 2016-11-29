3d0a84c63732248a752a95b7a124c7a4df010ecd
SalvaIsa <salvaisa7@libero.it>
2016-11-29 19:35:47 +0100

    New Floating Button added to MainTab1

--- app/src/main/java/com/mobiledev/salvaisa/globalchatroom/fragments/TabFragment2.java
+++ app/src/main/java/com/mobiledev/salvaisa/globalchatroom/fragments/TabFragment2.java
@@ -2,23 +2,26 @@ package com.mobiledev.salvaisa.globalchatroom.fragments;
 
 
 import android.os.Bundle;
+import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import com.github.clans.fab.FloatingActionButton;
 import com.mobiledev.salvaisa.globalchatroom.R;
+
+import java.util.LinkedList;
 
 /**
  * A simple {@link Fragment} subclass.
  */
 public class TabFragment2 extends Fragment {
 
+    private int mMaxProgress = 100;
+    private LinkedList<ProgressType> mProgressTypes;
 
-    public TabFragment2() {
-        // Required empty public constructor
-    }
-
+    public TabFragment2() {}
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
@@ -27,4 +30,78 @@ public class TabFragment2 extends Fragment {
         return inflater.inflate(R.layout.tab_fragment_2, container, false);
     }
 
+    @Override
+    public void onViewCreated(View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+        mProgressTypes = new LinkedList<>();
+        for (ProgressType type : ProgressType.values()) {
+            mProgressTypes.offer(type);
+        }
+
+        final FloatingActionButton fab = (FloatingActionButton) view.findViewById(R.id.fab);
+        fab.setMax(mMaxProgress);
+
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                ProgressType type = mProgressTypes.poll();
+                switch (type) {
+                    case INDETERMINATE:
+                        fab.setShowProgressBackground(true);
+                        fab.setIndeterminate(true);
+                        mProgressTypes.offer(ProgressType.INDETERMINATE);
+                        break;
+                    case PROGRESS_POSITIVE:
+                        fab.setIndeterminate(false);
+                        fab.setProgress(70, true);
+                        mProgressTypes.offer(ProgressType.PROGRESS_POSITIVE);
+                        break;
+                    case PROGRESS_NEGATIVE:
+                        fab.setProgress(30, true);
+                        mProgressTypes.offer(ProgressType.PROGRESS_NEGATIVE);
+                        break;
+                    case HIDDEN:
+                        fab.hideProgress();
+                        mProgressTypes.offer(ProgressType.HIDDEN);
+                        break;
+                    case PROGRESS_NO_ANIMATION:
+                        //increaseProgress(fab, 0);
+                        break;
+                    case PROGRESS_NO_BACKGROUND:
+                        fab.setShowProgressBackground(false);
+                        fab.setIndeterminate(true);
+                        mProgressTypes.offer(ProgressType.PROGRESS_NO_BACKGROUND);
+                        break;
+                }
+            }
+        });
+
+    }
+
+    /*
+    private void increaseProgress(final FloatingActionButton fab, int i) {
+        if (i <= mMaxProgress) {
+            fab.setProgress(i, false);
+            final int progress = ++i;
+            mUiHandler.postDelayed(new Runnable() {
+                @Override
+                public void run() {
+                    increaseProgress(fab, progress);
+                }
+            }, 30);
+        } else {
+            mUiHandler.postDelayed(new Runnable() {
+                @Override
+                public void run() {
+                    fab.hideProgress();
+                }
+            }, 200);
+            mProgressTypes.offer(ProgressType.PROGRESS_NO_ANIMATION);
+        }
+    }*/
+
+    private enum ProgressType {
+        INDETERMINATE, PROGRESS_POSITIVE, PROGRESS_NEGATIVE, HIDDEN, PROGRESS_NO_ANIMATION, PROGRESS_NO_BACKGROUND
+    }
 }
